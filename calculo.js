1c4e81a30ff03717a6849c4fb9831021f614f03c
DanTinho <161615116+DanTinho@users.noreply.github.com>
2024-10-06 18:25:31 -0300

    Add files via upload

--- calculo.js
+++ calculo.js
@@ -1,9 +1,33 @@
+var númeroDeInscritos = 0;
+
 function InscreverAluno() {
+
+    númeroDeInscritos++;
 
     aluno = prompt("Qual seu nome?");
     idadeAluno = prompt("Qual sua idade?");
 
     let resposta = window.document.getElementById('alunoNome');
+    let respostaIdade = window.document.getElementById('alunoIdade');
 
-    resposta.innerHTML = "<p>" + aluno + " " + idadeAluno + "</p>";
+
+    if(resposta){
+        resposta.innerHTML += "<p>" + aluno + "</p>";
+        respostaIdade.innerHTML += "<p>" + idadeAluno + "</p>";
+    } 
+}
+
+function CalcularTimes() {
+
+    let quantidadeDeTimes = parseInt(númeroDeInscritos / 3);
+    let resposta2 = window.document.getElementById('respostaTimes');
+
+    if (quantidadeDeTimes) {
+        resposta2.innerHTML = "A quantidade de times formados é " + quantidadeDeTimes + ". E nenhuma aluno sobrará.";
+        window.document.getElementById('respostaTimes').style.color = 'green';
+    } else {
+        resposta2.innerHTML = "A quantidade de times formados é " + quantidadeDeTimes + ". E sobrarão alunos.";
+        window.document.getElementById('respostaTimes').style.color = 'red';
+    }
+    
 }
